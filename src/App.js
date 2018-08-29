749ac0ebbbf1abf1cda6c8d45a13aacba24ffae8
Katari Ganesh kumar <katariganeshkumar@gmail.com>
2018-08-29 21:03:37 +0530

    29aug

--- src/App.js
+++ src/App.js
@@ -4,12 +4,19 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div >
-        <p> 
-          hi i'am ganesh kumar i will be back in a day....
-        </p>
-      </div>
-    );
+      
+      
+        <div class="mw5 center bg-white br3 pa3 pa4-ns mv3 ba b--black-10">
+        <div class="tc">
+          
+          <h1 class="f3 mb2">Ganesh Kumar Katari</h1>
+          <h2 class="f5 fw4 gray mt0">CEO of Keegaa.com</h2>
+        </div>
+        </div>
+
+        
+      
+    );  
   }
 }
 
